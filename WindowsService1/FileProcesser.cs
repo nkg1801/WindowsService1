73d0ae2b34b649fcf5b359d6a76f166dc823ba2f
Nand <nkg1801@gmail.com>
2021-12-31 10:49:35 +0530

    Add project files.

---
+++ WindowsService1/FileProcesser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WindowsService1
+{
+    public class FileProcesser
+    {
+        FileSystemWatcher watcher;
+        string directoryToWatch;
+        public FileProcesser(string path)
+        {
+            this.watcher = new FileSystemWatcher();
+            this.directoryToWatch = path;
+            watcher.IncludeSubdirectories = true;
+        }
+        public void Watch()
+        {
+            watcher.Path = directoryToWatch;
+            watcher.NotifyFilter = NotifyFilters.LastAccess |
+                         NotifyFilters.LastWrite |
+                         NotifyFilters.FileName |
+                         NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            //File.Copy(e.FullPath, ConfigurationManager.AppSettings["ToPath"] + "\\" + Path.GetFileName(e.FullPath), true);
+            string sourceFileName = Path.GetFileName(e.FullPath);
+            //string 
+            //C:\devops\asdf\New Text Document.txt
+            string[] token = e.FullPath.Split('\\');
+            token[1] = "devops2";
+            string dest = token[0];
+            //foreach(string str in token)
+            for(int i=1; i<token.Length; i++)
+            {
+                dest = dest + "\\" + token[i];
+            }
+            Debug.Print("Destination: " + dest);
+            Debug.Print("Soure file path: " + sourceFileName);
+            
+
+            FileAttributes attr = File.GetAttributes(e.FullPath);
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                Directory.CreateDirectory(dest);
+            }
+            else
+            {
+                File.Copy(e.FullPath, dest, true);
+            }
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        FileProcesser fp;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            fp = new FileProcesser("C:\\devops");
+            fp.Watch();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
